b0fa3494460d185f59abb0a86e5e2412b29b1adf
Mac Gaulin <gaulinmp+git@gmail.com>
2014-11-17 18:45:08 +0000

    Set static path for running from cron

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-#cd "$(dirname "$0")"
-cd /
+cd /home/pi/projects/kindle-weather-display/server
 
 python2 weather-script.py
 rsvg-convert --background-color=white -o weather-script-output-raw.png weather-script-output.svg
 pngcrush -c 0 weather-script-output-raw.png weather-script-output.png
-# cp -f weather-script-output.png /var/www/weather/weather-script-output.png
 scp weather-script-output.png kindle:/mnt/us/weather/  
