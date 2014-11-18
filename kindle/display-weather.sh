9dae860c798914362691d25dd3304bb52632ec3a
Mac Gaulin <gaulinmp+git@gmail.com>
2014-11-18 05:50:04 +0000

    Moved show.sh to display-weather.sh

--- kindle/display-weather.sh
+++ kindle/display-weather.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-cd "$(dirname "$0")"
+/usr/sbin/eips -c
+/usr/sbin/eips -c
 
-rm weather-script-output.png
-eips -c
-eips -c
-
-if wget http://server/path/to/weather-script-output.png; then
-	eips -g weather-script-output.png
-else
-	eips -g weather-image-error.png
-fi
+/usr/sbin/eips -g /mnt/us/weather/weather-script-output.png
+date | /usr/sbin/eips 20 1
--- server/weather-script.sh
+++ server/weather-script.sh
@@ -6,4 +6,4 @@ python2 weather-script.py
 rsvg-convert --background-color=white -o weather-script-output-raw.png weather-script-output.svg
 pngcrush -c 0 weather-script-output-raw.png weather-script-output.png
 scp weather-script-output.png kindle:/mnt/us/weather/  
-ssh kindle "/bin/sh /mnt/us/weather/show.sh"
+ssh kindle "/bin/sh /mnt/us/weather/display-weather.sh"
